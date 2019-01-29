9fb3c0bde03bc160fd38c96da3f0c4b91bc5f0d4
Hashmatullah Nazari <hn0153@gmail.com>
2019-01-29 07:57:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $(".toggle").click(function(){
+    $("ul").slideToggle("slow");
+  });
+});
+
+  function myFunction() {
+            location.reload();
+         }
